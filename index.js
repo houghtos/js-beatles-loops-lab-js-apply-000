09ac3d2d4aa9292741db9f2dc6a042965109c9e0
beatific-demo-1566 <sbn.houghton@gmail.com>
2019-01-16 03:59:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function theBeatlesPlay(musicians, instruments) {
-  var emptyArray = [];
+  var returnArray = [];
   //var len = musicians.length;
   for (var i = 0;  i < musicians.length; i++){
     var arrayAppend = musicians[i] + " plays " + instruments[i]
       emptyArray.push(arrayAppend)
   }
 
-  return emptyArray
+  return returnArray
 }
